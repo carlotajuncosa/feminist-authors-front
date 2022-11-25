dd1313e3123d027032b983ec8a984e04a391b887
Guillermo Chacón Poveda <guillermo.chacon@bootcamp-upgrade.com>
2022-11-25 21:21:40 +0100

    updated with small errors

--- src/app/components/contribute/contribute.component.ts
+++ src/app/components/contribute/contribute.component.ts
@@ -1,4 +1,8 @@
+import { AuthorsService } from './../../services/authors.service';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { IAuthors } from 'src/app/models/interfaces';
 
 @Component({
   selector: 'app-contribute',
@@ -6,10 +10,68 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contribute.component.css']
 })
 export class ContributeComponent implements OnInit {
-
-  constructor() { }
+  newAuthor: IAuthors ={
+    name: '',
+    birthdate: '',
+    deathdate: '',
+  };
+  authorsForm!: FormGroup;
+  
+  constructor(private authorService: AuthorService, private formBuilder: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
+    this.authorForm = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      birthdate: ['', [Validators.required, Validators.minLength(3)]],
+      deathdate: ['', [Validators.required]],
+  
+    })
+
+    this.authorsForm.valueChanges.subscribe((changes) => {
+      this.newAuthor = changes;
+    })
   }
 
 }
+
+
+/* export class NewCharacterComponent implements OnInit {
+  newCharacter: any = {
+    name: '',
+    race: '',
+    img: '',
+  };
+  characterForm!: FormGroup;
+
+  constructor(private characterService: CharactersService, private formBuilder: FormBuilder, private router: Router) {}
+
+  ngOnInit(): void {
+    this.characterForm = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      img: ['', [Validators.required, Validators.minLength(3)]],
+      race: ['', [Validators.required]],
+    })
+
+    this.characterForm.valueChanges.subscribe((changes) => {
+      this.newCharacter = changes;
+    })
+  }
+  onFileChange(event:any){
+    const file = event.target.files[0];
+    console.log(file)
+    this.characterForm.patchValue({
+      img: file
+    })
+
+  }
+ onSubmit() {
+    const formData = new FormData();
+    formData.append('img', this.characterForm.get('img')?.value);
+    formData.append('name', this.characterForm.get('name')?.value);
+    formData.append('race', this.characterForm.get('race')?.value);
+    this.characterService.postCharacter(formData).subscribe()
+    this.router.navigate(["/characters"])
+  }
+}
+
+ */

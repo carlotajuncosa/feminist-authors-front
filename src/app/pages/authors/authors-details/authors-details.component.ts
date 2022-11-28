5cbdfd001dd71f1511d62580400ef95ab922621b
carlotajuncosa <carlota.juncosa@gmail.com>
2022-11-28 10:38:08 +0100

    Styles main

--- src/app/pages/authors/authors-details/authors-details.component.ts
+++ src/app/pages/authors/authors-details/authors-details.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-authors-details',
   templateUrl: './authors-details.component.html',
-  styleUrls: ['./authors-details.component.css']
+  styleUrls: ['./authors-details.component.scss']
 })
 export class AuthorsDetailsComponent implements OnInit {
   id?: any;
